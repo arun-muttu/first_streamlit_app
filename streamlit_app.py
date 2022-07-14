421892ad6474803717b9aeb8712756fd9fdf607a
muttupublic <109307878+muttupublic@users.noreply.github.com>
2022-07-14 21:03:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('👨🏽‍🌾 My Parents New Healthy Diner 👩🏽‍🍳')
+streamlit.title('👨🏽‍🌾 My Parents Healthy Diner 👩🏽‍🍳')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
