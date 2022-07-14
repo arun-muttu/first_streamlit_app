e052e26bca4d4f26f9ff20422e34ccdb791811d3
muttupublic <109307878+muttupublic@users.noreply.github.com>
2022-07-14 21:04:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title('👨🏽‍🌾 My Parents Healthy Diner 👩🏽‍🍳')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
