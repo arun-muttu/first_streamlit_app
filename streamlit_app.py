eeb5c0a8b04a5a693dddb91429d2ad1f3e7f940d
muttupublic <109307878+muttupublic@users.noreply.github.com>
2022-07-15 21:58:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,5 +31,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-for fruit in [fruits_selected]:
+for fruit in [fruits_to_show]:
   streamlit.text('fruit')
